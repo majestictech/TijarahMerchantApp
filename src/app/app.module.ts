a20627c4c2ae9c0f1253e3915178203c782f29cf
jpanchal01 <117058172+jpanchal01@users.noreply.github.com>
2023-01-27 14:07:27 -0800

    Edit vendor

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,16 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
-
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TranslateModule, TranslateLoader } from '@ngx-translate/core';
 import { HttpClientModule, HttpClient } from '@angular/common/http';
 import { TranslateHttpLoader } from '@ngx-translate/http-loader';
 import { TranslateConfigService } from './services/translate-config.service';
+import { BrowserModule } from '@angular/platform-browser';
+import { DatePipe, registerLocaleData } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule} from '@angular/forms'; 
 
 
 export function LanguageLoader(http: HttpClient) {
@@ -27,7 +28,7 @@ export function LanguageLoader(http: HttpClient) {
         useFactory: (LanguageLoader),
         deps: [HttpClient]
       }
-    })],
+    }),ReactiveFormsModule,FormsModule],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy },TranslateConfigService],
   bootstrap: [AppComponent],
 })
--- src/app/vendorlist/vendorlist.page.ts
+++ src/app/vendorlist/vendorlist.page.ts
@@ -1,15 +1,51 @@
-import { Component, OnInit } from '@angular/core';
-
+import { InvoicesService } from '../services/invoices.service';
+import { EnvService } from '../services/env.service';
+import { Observable } from 'rxjs';
+import { Component, OnInit,ViewChild,ElementRef  } from '@angular/core';
+import { IonSearchbar } from '@ionic/angular';
 @Component({
   selector: 'app-vendorlist',
   templateUrl: './vendorlist.page.html',
   styleUrls: ['./vendorlist.page.scss'],
 })
 export class VendorlistPage implements OnInit {
+	vendororderlist: Observable<any>;
+	vendorCount: number = 0;
+	searchText: string = '';
+  
+  @ViewChild('searchVendors', { static: false }) searchbar: IonSearchbar;
 
-  constructor() { }
+  constructor(public env: EnvService,private invoicesService: InvoicesService) { }
 
   ngOnInit() {
   }
+  loadData() 
+	{
+		this.vendororderlist = this.invoicesService.vendorSelect(this.searchText);
+		this.vendororderlist.subscribe(res => {
+			console.log(res);		
+			this.vendorCount = res.length;
+			
+		});
+	}
 
+  doRefresh(event) {
+		this.loadData();
+		event.target.complete();
+	}
+	
+	ionViewWillEnter() {
+		this.loadData();
+  }
+  
+  async ionViewDidEnter() {
+	this.env.hideLoader();
+  }
+
+  getItems(ev: any) {
+	const val = ev.target.value;
+	
+	this.searchText = val;
+	this.loadData();
+  }
 }
